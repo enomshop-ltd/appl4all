d7b8f346a323be44b3eb582be9f89c3b1365a79d
Nelly #1 <122103189+enomshop-ltd@users.noreply.github.com>
2025-11-16 09:40:36 +0000

    ff

--- routes/shop/api/v1/create.ts
+++ routes/shop/api/v1/create.ts
@@ -1,6 +1,4 @@
-import { client, define } from "../../../../utils.ts";
-import { z } from "zod";
-import { parseForm } from "@jmondi/zod-friendly-forms";
+//import { client, define } from "../../../../utils.ts";
 
 export const handler = define.handlers({
   async POST(ctx) {
@@ -14,17 +12,17 @@ export const handler = define.handlers({
     const Condition = form.get("Condition");
     const SKU = form.get("SKU");
 
-    await client.connect();
-    const db = client.db("enomshop");
-    const collection = db.collection("shop");
-    const res = await collection.insertOne(item);
+    //await client.connect();
+    //const db = client.db("enomshop");
+    //const collection = db.collection("shop");
+    //const res = await collection.insertOne(item);
 
-    await client.close();
+    //await client.close();
 
     return Response.json({
       res: "ok",
       message: "Product created",
-      id: res.insertedId,
+      //id: res.insertedId,
     });
   },
 });
